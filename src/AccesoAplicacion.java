0e2cf687f21ee46eb22a7d6d684100338d4b068a
Jesus Matilla León <jesusway69@gmail.com>
2022-08-01 13:43:54 +0200

    primer ejercicio while

---
+++ src/AccesoAplicacion.java
@@ -0,0 +1,21 @@
+
+import javax.swing.*;
+
+public class AccesoAplicacion {
+
+    public static void main(String[] args) {
+
+        String clave = "Jesus";
+        String password = "";
+        while (clave.equals(password) == false) {
+            password = JOptionPane.showInputDialog("Introduzca clave");
+            if (clave.equals(password) == false) {
+                System.out.println("Clave incorrecta");
+
+            }
+        }
+        System.out.println("Clave correcta");
+
+    }
+
+}
