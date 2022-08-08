be34a93b5c4807315ff5c9453e9f687588f6cc55
jesus <jesus@DESKTOP-IB8IFH5>
2022-08-08 18:34:53 +0200

    ejercicios 20220806

--- src/poo/Coche.java
+++ src/poo/Coche.java
@@ -1,19 +1,39 @@
 package poo;
 
 public class Coche {
+//variables de instancia
 
-    int ruedas;
-    int largo;
-    int ancho;
-    int motor;
-    int peso;
+    private int ruedas;
+    private int largo;
+    private int ancho;
+    private int motor;
+    private int peso_plataforma;
+    String color;
+    int peso_total;
+    boolean asientos_cuero, climatizador;
 
+
+    /*private es un modificador de acceso que encapsula el dato
+ para que no se pueda modificar en otra clase*/
     public Coche() {
+        //constructores
         ruedas = 4;
         largo = 2000;
         ancho = 300;
         motor = 1500;
-        peso = 500;
+        peso_plataforma = 500;
     }
 
+    public String dime_largo() { //método getter que nos devuelve un valor con texto
+        return "El coche mide " + largo + " de largo";
+    }
+
+    public void establece_color() { //método setter que establece valor y no devuelve dato (void)
+        color = "azul";
+
+    }
+    
+    public String dime_color(){//método getter que nos devuelve un valor con texto
+        return "el color del coche es " + color;
+    }
 }
--- src/poo/UsoCoche.java
+++ src/poo/UsoCoche.java
@@ -6,11 +6,12 @@ public class UsoCoche {
   
     public static void main(String[] args) {
         
-        Coche Renault = new Coche(); //INSTANCIAR UNA CLASE, EJEMPLAR DE CLASE
-        
-        System.out.println("Este coche tiene " + Renault.ruedas + " ruedas");
+        Coche Fiat = new Coche(); //INSTANCIAR UNA CLASE, EJEMPLAR DE CLASE
+        Fiat.establece_color();//llamamos al método setter
+        //System.out.println("Este coche tiene " + Fiat.ruedas + " ruedas");
 
-
+System.out.println("Este coche pesa " + Fiat.peso_total + " kilos");
+ System.out.println(Fiat.dime_color());
 
     }
 
